b460fbae22891108d53a159348142f5c8ba94bc7
longxiang1025 <longxiang1025@gmail.com>
2017-03-17 01:49:37 -0400

    create_plot_R

--- Analyticalcurve.R
+++ Analyticalcurve.R
@@ -1,9 +1,9 @@
 
 n<-2^16
 setwd("C:/Users/H00037258/OneDrive/Documents/17 Spring/CS124/PA2")
-
-result<-matrix(NA,ncol = 6,nrow = 100)
-for(s in 4:100)
+len=12
+result<-matrix(NA,ncol = 7,nrow = len)
+for(s in 4:len)
 {
   n<-2^s
   ##pure strassen algorithm
@@ -18,6 +18,20 @@ for(s in 4:100)
   time5<-((7/4)^(log2(n/8))-1)*6*n^2+7^(log2(n/8))*(8^2*(8*8-1))
   #basic cache=16*16
   time6<-((7/4)^(log2(n/16))-1)*6*n^2+7^(log2(n/16))*(16^2*(16*16-1))
-  result[s,]<-c(time1,time3,time4,time5,time6,time2)
-  print(s)
+  result[s,2:7]<-c(time1,time3,time4,time5,time6,time2)
+  #result[s,2:7]<-log2(result[s,2:7])
+  result[s,1]<-n
 }
+result<-as.data.frame(result)
+names(result)<-c("n","Strassen's","2*2","4*4","8*8","16*16","Conventional")
+data_long <- melt(result, id="n")  
+names(data_long)[3]<-"time"
+library(ggplot2)
+library(data.table)
+ggplot(data=data_long,
+       aes(x=n, y=time, colour=variable,size=5)) +
+  geom_line(size=2)+
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold"))
+library(xtable)
+print(xtable(result))
